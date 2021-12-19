c8ea5733d4b99dbeb407a532fc90bdc57b539400
Matthieu Rousseau <matthieurousseau86@gmail.com>
2021-12-19 23:57:52 +0100

    fixed bug to return all companies

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 # pylint: disable=missing-docstring
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 app = Flask(__name__)
 
 PRODUCTS = {
@@ -12,13 +12,27 @@ PRODUCTS = {
         'id': 2,
         'name': 'Socialive.tv'
     },
+    3: {
+        'id': 3,
+        'name': 'Le Wagon'
+    },
 }
+
 
 @app.route('/')
 def hello():
     return "Hi World!"
 
 @app.route('/api/v1/products')
-def products():
-    return jsonify(PRODUCTS)
+def index_products():
+    return jsonify(list(PRODUCTS.values()))
 
+@app.route('/api/v1/products/<int:product_id>')
+def show_product(product_id):
+    product = PRODUCTS.get(product_id)
+
+    if product is None:
+        # abort(404)
+        return "Product not found", 404
+
+    return jsonify(product), 200
