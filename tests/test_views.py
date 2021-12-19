af1316336a20c2296cd8f7f817228da75bae8a83
Matthieu Rousseau <matthieurousseau86@gmail.com>
2021-12-20 00:20:07 +0100

    added 2 tests for the read product endpoint

--- tests/test_views.py
+++ tests/test_views.py
@@ -10,5 +10,20 @@ class TestViews(TestCase):
     def test_read_many_products(self):
         response = self.client.get("/api/v1/products")
         products = response.json
+        self.assertEqual(response.status_code, 200)
         self.assertIsInstance(products, list)
         self.assertGreater(len(products), 2)  # 2 is not a mistake here.
+
+    def test_read_one_product(self):
+        response = self.client.get("/api/v1/products/1")
+        product = response.json
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(product, dict)
+        self.assertEqual(list(product.keys()), ['id', 'name'])
+        self.assertEqual(product['name'], 'Skello')
+
+    def test_read_one_product_not_found(self):
+        response = self.client.get("/api/v1/products/100")
+        product = response.json
+        self.assertEqual(response.status_code, 404)
+        self.assertIsNone(product)
